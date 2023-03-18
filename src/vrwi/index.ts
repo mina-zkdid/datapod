92602f4643b8459114125fe5efa8cc3d2a09644b
Jerry Wong <yuki.n@tuta.io>
2023-03-18 18:10:24 +0800

    temp

--- src/vrwi/index.ts
+++ src/vrwi/index.ts
@@ -1,16 +1,32 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { readFile } from 'fs/promises';
 import { Buffer } from 'buffer';
-import { Circuit, Experimental, Field } from 'snarkyjs';
+import {
+  Circuit,
+  Experimental,
+  Field,
+  Struct,
+  CircuitString,
+  PublicKey,
+} from 'snarkyjs';
 
+const buf = Buffer.from('hello');
+
+const createDynamicStruct = (buf: Buffer) => {
+  const structLen = buf.byteLength;
+  Field.fromBytes(buf.toJSON().data);
+  //   const struct = Struct();
+  //   return struct;
+};
+/**
+ * # Verifiable Reading/Writing Interface (VRWI)
+ */
 export const VRWI = Experimental.ZkProgram({
   publicInput: Field,
   methods: {
     read: {
       privateInputs: [],
-      method() {
-        Circuit.runAndCheck(() => {});
-      },
+      method() {},
     },
   },
 });
